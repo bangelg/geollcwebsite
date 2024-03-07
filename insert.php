388911015df0accb3f1efed0687fd669f0b11f2b
bangelg <angelbriang@gmail.com>
2024-03-07 01:29:40 -0800

    adding test versions

--- insert.php
+++ insert.php
@@ -8,50 +8,27 @@
 <body>
 	<center>
 		<?php
+        $serverName = "serverName";
+        $options = array(  "UID" => "0E44AAF9-75ED-4E6B-B0F6-FCF5F95C656D",  "PWD" => "Lester1809nine",  "Database" => "Test");
+        $conn = sqlsrv_connect($serverName, $options);
+        
+        if( $conn === false )
+             {
+             echo "Could not connect.\n";
+             die( print_r( sqlsrv_errors(), true));
+             }
+        
+        $name = $_POST['name'];
+        $age= $_POST['age'];
+        
+        $query = "INSERT INTO dbo.small
+                (Name_,Age)
+                VALUES(?, ?)";
+        $params1 = array($name,$age);                       
+        $result = sqlsrv_query($conn,$query,$params1);
 
-        $server = 'wesbitedb.cv2im0m26jl5.us-west-1.rds.amazonaws.com';
-        $database = 'Main';
-        $username = 'admin';
-        $password = 'Lester1809nine';
-
-		$conn = new PDO("sqlsrv:server=$server;Database=$database", $username, $password);
-		
-		// Check connection
-		if($conn === false){
-			die("ERROR: Could not connect. "
-				. mysqli_connect_error());
-		}
-		
-		// Taking all 5 values from the form data(input)
-		$project_name = $_REQUEST['project_name'];
-		$boring_id = $_REQUEST['boring_id'];
-		$sample_number = $_REQUEST['sample_number'];
-		$depth = $_REQUEST['depth'];
-		$bag_tube_number = $_REQUEST['bag_tube_number'];
-        $test_name = $_REQUEST['test_name'];
-		$notes = $_REQUEST['notes'];
-		$progress = $_REQUEST['progress'];
-		
-		// Performing insert query execution
-		// here our table name is college
-        try{
-		$query = "INSERT INTO Samples (project_name, boring_id, sample_number, depth, bag_tube_number, test_name, notes, progress)
-        VALUES (:project_name,:boring_id,:sample_number,:depth,:bag_tube_number,:test_name,:notes,:progress)";
-		$statement = $connection->prepare($query);
-        $statement->bindValue(':project_name',	$project_name );
-        $statement->bindValue(':boring_id', $boring_id);
-        $statement->bindValue(':sample_number', $sample_number);
-        $statement->bindValue(':depth', $depth);
-        $statement->bindValue(':bag_tube_number', $bag_tube_number);
-        $statement->bindValue(':test_name', $test_name);
-        $statement->bindValue(':notes', $notes);
-        $statement->bindValue(':progress', $progress);
-        $statement->execute();
-        }catch(PDOException $e) {
-            echo 'Insert Failed! Code: ' . $e->getMessage();
-        }
-		
-
+        echo 'Inserted Data!'
+        sqlsrv_close($conn);
 		?>
 	</center>
 </body>

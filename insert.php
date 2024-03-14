2722a1fea023259493d3f87a03a639d120d93dac
bangelg <angelbriang@gmail.com>
2024-03-13 20:22:37 -0700

    changed insert file

--- insert.php
+++ insert.php
@@ -8,27 +8,29 @@
 <body>
 	<center>
 		<?php
-        $serverName = "serverName";
-        $options = array(  "UID" => "0E44AAF9-75ED-4E6B-B0F6-FCF5F95C656D",  "PWD" => "Lester1809nine",  "Database" => "Test");
-        $conn = sqlsrv_connect($serverName, $options);
-        
-        if( $conn === false )
-             {
-             echo "Could not connect.\n";
-             die( print_r( sqlsrv_errors(), true));
-             }
-        
-        $name = $_POST['name'];
-        $age= $_POST['age'];
-        
-        $query = "INSERT INTO dbo.small
-                (Name_,Age)
-                VALUES(?, ?)";
-        $params1 = array($name,$age);                       
-        $result = sqlsrv_query($conn,$query,$params1);
 
-        echo 'Inserted Data!'
-        sqlsrv_close($conn);
+        $dsn = 'dblib:host=aws-endpoint.rds.amazonaws.com:1433;dbname=Test;';
+        $user = 'Admin';
+        $password = 'Lester1809nine';
+        
+        try {
+        
+        $dbh = new PDO($dsn, $user, $password);
+        
+        $result = $dbh->query("SELECT * 
+            FROM table-name;");
+        
+        foreach ($result as $row) {
+            echo '<pre>';
+            print_r($row);
+            echo '</pre>';
+        }
+        
+        } catch (PDOException $e) {
+        
+        echo 'Connection failed: ' . $e->getMessage();
+        
+        }
 		?>
 	</center>
 </body>

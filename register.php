dc22799d4515522a0f98c32a9f5905c91141cd22
bangelg <angelbriang@gmail.com>
2024-05-04 01:48:02 -0700

    changing php insetion

--- register.php
+++ register.php
@@ -8,16 +8,22 @@
         }
 
         set_exception_handler('exception_handler');
+        
     if ($conn->connect_error) {
         die('Connect Error: ' . $conn->connect_error);
     } else {
         if(isset($_POST['submit'])){
 
+            $stmt = $conn->prepare("INSERT INTO users (username, email, passwrd)
+            VALUES (?, ?, ?)");
+            $stmt->bind_param("sss",$username, $email, $pass);
+   
+
             $user = mysqli_real_escape_string($conn, $_POST['usernameInp']);
             $email = mysqli_real_escape_string($conn, $_POST['emailInp']);
             $pass = hash($_POST['passwordInp']);
            
-            $select = " SELECT * FROM users WHERE email = '$email' && password = '$pass' ";
+            $select = " SELECT * FROM users WHERE email = '$email' && passwrd = '$pass' ";
          
             $result = mysqli_query($conn, $select);
          
@@ -30,8 +36,7 @@
                if($pass != $cpass){
                   $error[] = 'password not matched!';
                }else{
-                  $insert = "INSERT INTO users(username, email, password) VALUES('$user','$email','$pass')";
-                  mysqli_query($conn, $insert);
+                  $stmt->execute();
                   header('location:login.html');
                }
             }

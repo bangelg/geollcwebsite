c177a4e9036912509dbf2f8ec51ffa3c779431f8
bangelg <angelbriang@gmail.com>
2024-06-06 17:19:21 -0700

    adding error messages for QRGen

--- QRGen.php
+++ QRGen.php
@@ -2,6 +2,22 @@
 session_start();
 @include("config.php");
 @include("checkSession.php");
+
+if(isset($_POST['submit'])){
+
+  $boring_id = mysqli_real_escape_string($conn, $_POST['boring_id']);
+
+  $select = mysqli_query($conn, "SELECT * FROM `Samples` WHERE Boring_ID = '$boring_id'") or die('query failed');
+
+  if(mysqli_num_rows($select) == 0){
+      header('location:insert.php');
+      exit();
+  } else {
+    $error[] = 'Boring ID currently exists.'; 
+  }
+};
+?>
+
 ?>
 
 <!DOCTYPE html>
@@ -15,8 +31,8 @@ session_start();
   <meta charset="utf-8" />
   <meta name="viewport" content="initial-scale=1, width=device-width" />
 
-  <link rel="stylesheet" href="css/global.css" />
-  <link rel="stylesheet" href="css/QRGen.css" />
+  <link rel="stylesheet" href="./global.css" />
+  <link rel="stylesheet" href="./QRGen.css" />
   <link
     rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap"
@@ -28,10 +44,19 @@ session_start();
 </head>
 
     <div class="container">
-      <form action="insert.php" method="POST" id="results">
+      <form action="" method="POST" id="results">
          <div class="labels">
+          <?php
+          if (isset($error)) {
+              foreach ($error as $err) {
+                  echo "<p style='color:red;'>$err</p>";
+              }
+          }
+          ?>
+        <br>
           <label id="name-label" for="projectName"> Project Name</label></div>
         <div class="input-tab">
+          <br>
           <input class="input-field" type="text" id="projectName" name="project_name" placeholder="Enter the project name." required autofocus></div>
         <div class="labels">
           <label id="number-label" for="boringId"> Boring ID</label></div>
@@ -57,7 +82,7 @@ session_start();
           <label id="name-label" for="LNotes"> Notes </label></div>
         <div class="input-tab">
           <input class="input-field" type="text" id="LNotes" name="notes" placeholder="Enter any notes." required autofocus></div>
-             
+        
         
         <div class="labels">
           <label>Progress?</label></div>
@@ -70,7 +95,6 @@ session_start();
         <div class="btn">
           <button id="submit" type="submit">Submit</button>
         </div>
-        
       </form>
     </div>
 </footer>
